0b46ee7268f3ab51eca8907a41f3867e0952224e
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-12 08:56:12 -0600

    Passes all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [number * number for number in int_list]
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    print_val = ""
+    for i in range(1,101):
+        if (i % 3) == 0:
+            print_val ="Fizz"
+        if (i % 5) == 0:
+            print_val += "Buzz"
+        if print_val == "":
+            print_val = i
+        print(print_val)
+        print_val = ""
+
